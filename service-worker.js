3aef733ce71cca81b4369ea0f640886085cdaefc
pbascoy67 <66634447+pbascoy67@users.noreply.github.com>
2025-01-24 17:44:41 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'open-key-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/logo.webp',
+  '/Centauro3.png', // Asegúrate de incluir la ruta correcta
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css',
